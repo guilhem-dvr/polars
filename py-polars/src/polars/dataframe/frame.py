16a1541c45af4dc9b433f1cc2a28e63650265fbd
Guilhem de Viry <gdeviry@mytraffic.fr>
2024-11-07 00:59:59 +0100

    use shorthand for unlimited rows

--- py-polars/src/polars/dataframe/frame.py
+++ py-polars/src/polars/dataframe/frame.py
@@ -12615,7 +12615,7 @@ class DataFrame:
         """
         if limit is None:
             df = self
-            tbl_rows = self.height
+            tbl_rows = -1
         else:
             df = self.head(limit)
             if limit < 0:
